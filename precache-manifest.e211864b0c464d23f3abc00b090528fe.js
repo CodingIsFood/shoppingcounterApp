a33f67fabc6aa3fed7cfec7954242bcce148abd9
CodingIsFood <ayoodedere@gmail.com>
2019-02-23 23:02:05 +0100

    Updates

---
+++ precache-manifest.e211864b0c464d23f3abc00b090528fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "975233dc9a2343084115",
+    "url": "/shoppingcounterApp/static/css/main.e45e7b18.chunk.css"
+  },
+  {
+    "revision": "975233dc9a2343084115",
+    "url": "/shoppingcounterApp/static/js/main.975233dc.chunk.js"
+  },
+  {
+    "revision": "85f9005ce03e7818e58c",
+    "url": "/shoppingcounterApp/static/css/1.e49204c8.chunk.css"
+  },
+  {
+    "revision": "85f9005ce03e7818e58c",
+    "url": "/shoppingcounterApp/static/js/1.85f9005c.chunk.js"
+  },
+  {
+    "revision": "ea52ae71a46fee28f38e",
+    "url": "/shoppingcounterApp/static/js/runtime~main.ea52ae71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppingcounterApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "97a4251fe5348f7d4e9f1b527ce3bd9f",
+    "url": "/shoppingcounterApp/index.html"
+  }
+];
