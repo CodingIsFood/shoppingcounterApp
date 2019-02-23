af2c21ef70481fa2dbc46e4e7d70c4ca5236d7e4
CodingIsFood <ayoodedere@gmail.com>
2019-02-23 23:05:37 +0100

    minor edit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
 
 state={
     counters: [
-       {id: 1, value: 0},
+       {id: 1, value: 1},
        {id: 2, value: 3},
-       {id: 3, value: 0},
-       {id: 4, value: 7}  
+       {id: 3, value: 1},
+       {id: 4, value: 7}
     ]
 
   }
